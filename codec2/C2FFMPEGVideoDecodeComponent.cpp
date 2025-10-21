246b72dfb643e172763d688da68544408a2e8e67
SupeChicken666 <me@supechicken666.dev>
2025-10-21 15:10:25 +0000

    codec2: Skip releaseBufferVAAPI when decoder is deinitializing
    Signed-off-by: SupeChicken666 <me@supechicken666.dev>

--- codec2/C2FFMPEGVideoDecodeComponent.cpp
+++ codec2/C2FFMPEGVideoDecodeComponent.cpp
@@ -1289,6 +1289,8 @@ int C2FFMPEGVideoDecodeComponent::getBufferVAAPI(AVHWFramesContext* hwfc, AVFram
 }
 
 void C2FFMPEGVideoDecodeComponent::releaseBufferVAAPI(VASurfaceID surfaceId) {
+    if (mHeldSurfaces.size() == 0) return;
+
     auto it = mHeldSurfaces.find(surfaceId);
 
     if (it == mHeldSurfaces.end()) {
